74253ded472da7097ceac62ccf44f9cdf7934913
sqrt3 <1dimchae@gmail.com>
2024-10-15 10:15:15 +0900

    Initial Commit

---
+++ src/main/java/com/estsoft/springproject/service/MemberService.java
@@ -0,0 +1,27 @@
+package com.estsoft.springproject.service;
+
+import com.estsoft.springproject.entity.Member;
+import com.estsoft.springproject.repository.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository repository;
+
+//    public MemberService(MemberRepository repository) { // DI
+//        this.repository = repository;
+//    }
+
+    public List<Member> getAllMembers() {
+        return repository.findAll();        // Member 테이블 모든 정보 조회
+        // SELECT * FROM member;
+    }
+
+    public Member saveMember(Member member) {
+        return repository.save(member);
+    }
+
+}
