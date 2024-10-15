ff56180e514b4df453b42eece0ac3e920266c42e
sqrt3 <1dimchae@gmail.com>
2024-10-15 13:19:31 +0900

    Commit: Test 실습

---
+++ src/test/java/com/estsoft/springproject/JUnitTest.java
@@ -0,0 +1,21 @@
+package com.estsoft.springproject;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+    @Test
+    public void test() {
+        // given ( Optional )
+        int a = 1;
+        int b = 2;
+
+        // when: 검증하고 싶은 메서드 호출
+        int sum = a + b;
+
+        // then
+        //Assertions.assertEquals(3, sum);
+        Assertions.assertThat(sum).isEqualTo(3);
+        Assertions.assertThat(sum).isOdd();
+    }
+}
